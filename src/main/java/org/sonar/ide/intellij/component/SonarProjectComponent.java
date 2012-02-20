dc164e08cea6e97a9dc261934ac37a1979db23a8
maxtomin <maxim.tomin@gmail.com>
2012-02-20 18:39:00 +0000

    Added project-level host settings (storage only)

--- src/main/java/org/sonar/ide/intellij/component/SonarProjectComponent.java
+++ src/main/java/org/sonar/ide/intellij/component/SonarProjectComponent.java
@@ -5,4 +5,12 @@ import org.sonar.ide.intellij.model.ToolWindowModel;
 public interface SonarProjectComponent {
   void setToolWindowModel(ToolWindowModel model);
   ToolWindowModel getToolWindowModel();
+  SonarProjectState getState();
+
+  class SonarProjectState {
+    public String host;
+    public String user;
+    public String password;
+    public boolean configured;
+  }
 }
--- src/main/java/org/sonar/ide/intellij/component/SonarProjectComponentImpl.java
+++ src/main/java/org/sonar/ide/intellij/component/SonarProjectComponentImpl.java
@@ -1,6 +1,9 @@
 package org.sonar.ide.intellij.component;
 
+import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -8,9 +11,11 @@ import org.jetbrains.annotations.NotNull;
 import org.sonar.ide.intellij.listener.SonarFileEditorManagerListener;
 import org.sonar.ide.intellij.model.ToolWindowModel;
 
-public class SonarProjectComponentImpl implements SonarProjectComponent, ProjectComponent {
+@State(name = "SonarConfiguration", storages = {@Storage(id = "other", file = "$PROJECT_FILE$")})
+public class SonarProjectComponentImpl implements SonarProjectComponent, ProjectComponent, PersistentStateComponent<SonarProjectComponent.SonarProjectState> {
   private ToolWindowModel toolWindowModel;
   private Project project;
+  private SonarProjectState state = new SonarProjectState();
 
   public SonarProjectComponentImpl(Project project) {
     this.project = project;
@@ -50,5 +55,14 @@ public class SonarProjectComponentImpl implements SonarProjectComponent, Project
   @Override
   public void setToolWindowModel(ToolWindowModel model) {
     this.toolWindowModel = model;
+  }
+
+  public SonarProjectState getState() {
+    return state;
+  }
+
+  @Override
+  public void loadState(SonarProjectState state) {
+    this.state = state;
   }
 }
